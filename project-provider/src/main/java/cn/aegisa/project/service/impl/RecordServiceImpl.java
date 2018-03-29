89d28725686b2c5da945866c79c29923048048e0
xianyingda <daegis@yeah.net>
2018-03-29 14:39:19 +0800

    1dqwd

--- project-provider/src/main/java/cn/aegisa/project/service/impl/RecordServiceImpl.java
+++ project-provider/src/main/java/cn/aegisa/project/service/impl/RecordServiceImpl.java
@@ -8,7 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Using IntelliJ IDEA.
@@ -43,7 +47,51 @@ public class RecordServiceImpl implements RecordService {
     @Override
     public ValueResponse queryData() {
         ValueResponse response = new ValueResponse();
-        
+        List<WeightRecord> recordList = commonService.getList(WeightRecord.class);
+        // 获取今天
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime _15dayAgo = today.minusDays(15);
+        // 只看最近十五天的记录
+        recordList.removeIf(o -> o.getDatetime().isBefore(_15dayAgo));
+        // 获取到日期的集合
+        List<LocalDate> localDateList = recordList.stream().map(WeightRecord::getDatetime).map(LocalDateTime::toLocalDate).collect(Collectors.toList());
+        // 获取到唯一日期
+        Set<LocalDate> localDateSet = new TreeSet<>(localDateList);
+        DateTimeFormatter MMdd = DateTimeFormatter.ofPattern("MMdd");
+        List<String> strDay = localDateSet.stream().map(o -> o.format(MMdd)).collect(Collectors.toList());
+        response.setDays(strDay);
+        int dayCount = strDay.size();
+        log.info("需要展示{}天的记录", dayCount);
+        List<WeightRecord> xydRecordList = recordList.stream().filter(o -> o.getName().equals("yingda")).collect(Collectors.toList());
+        List<WeightRecord> shuangRecordList = recordList.stream().filter(o -> o.getName().equals("shuang")).collect(Collectors.toList());
+        List<Double> xydList = new LinkedList<>();
+        List<Double> shuangList = new LinkedList<>();
+        for (LocalDate localDate : localDateSet) {
+            WeightRecord xyd = getOneByDay(xydRecordList, localDate);
+            WeightRecord shuang = getOneByDay(shuangRecordList, localDate);
+            if (xyd != null) {
+                xydList.add(xyd.getWeight());
+            } else {
+                xydList.add(90.0);
+            }
+            if (shuang != null) {
+                shuangList.add(shuang.getWeight());
+            } else {
+                shuangList.add(50.0);
+            }
+        }
+        response.setXydValue(xydList);
+        response.setCoolValue(shuangList);
         return response;
+    }
+
+    private static WeightRecord getOneByDay(List<WeightRecord> list, LocalDate localDate) {
+        for (WeightRecord weightRecord : list) {
+            LocalDate dbDate = weightRecord.getDatetime().toLocalDate();
+            if (dbDate.isEqual(localDate)) {
+                return weightRecord;
+            }
+        }
+        return null;
     }
 }
